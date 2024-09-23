0b10cdc5f7be5518ac0ae67a47043d0e5a539bf6
Christian <christian.gyde@gmail.com>
2024-09-23 16:21:03 +0200

    Uploading Images with Cloudinary Part 1 and bug fixes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -8,7 +8,8 @@ const {
 const catchAsync = require('../utilities/catchAsync');
 const campgrounds = require('../controllers/campgrounds');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router
 	.route('/')
@@ -19,10 +20,8 @@ router
 		catchAsync(campgrounds.createCampground)
 	); */
 	.post(upload.array('image'), (req, res) => {
-		res.send({
-			body: req.body,
-			file: req.files,
-		});
+		console.log(req.body, req.files);
+		res.send('it worked');
 	});
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
